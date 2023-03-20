b8dd3f66a7ac9fc1e27b0f9f542aa4310f1d891e
Neto Geremias <netogeremias019@gmail.com>
2023-03-20 20:41:22 +0000

    basic of ejs

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
-const app = expres();
+const app = express();
 
-app.set('viw engine', 'ejs');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get("/", function(req, res){
-    res.send("Welcome to our web page");
+    var nome = "Jhon Doe";
+    var showTheMsg = false;
+
+    var product = [
+        {name: "Milk", price: 1.99},
+        {name: "Biscuit", price: 3.85},
+        {name: "Meat", price: 99.55},
+        {name: "kniff", price: 0.96},
+    ]
+    
+    res.render("index", {
+        nome: nome,
+        data: 20032023,
+        showTheMsg,
+        product,
+    });
 });
 
 app.listen(3000, ()=>{console.log("You started the server on http:localhost:3000");});
